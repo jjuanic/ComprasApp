c258fd5209fd5022214ad095609c95389934279d
Juan Ignacio Chiappella Zarini <juanignacioch4@gmail.com>
2024-05-31 02:43:53 -0300

    fix inner join -> left join

--- back/services/rubroProveedorService.js
+++ back/services/rubroProveedorService.js
@@ -26,7 +26,6 @@ async function selectRubroProveedor() {
 
 const selectProveedoresYRubros = async () => {
     try {
-        // Ejecutar la consulta SQL
         const result = await client.query(`
             select p.*,r.idrubro,r.nombre from proveedor p
             left join rubroProveedor rp on rp.idproveedor = p.idproveedor 
@@ -34,7 +33,7 @@ const selectProveedoresYRubros = async () => {
             order by p.idproveedor ASC
         `);
 
-        // Manipular los resultados para estructurarlos como un objeto JSON
+        // Estructuro los resultados para json
         const proveedoresConRubros = {};
         result.rows.forEach(row => {
             const { idproveedor, nombre, numerotelefono, codpostal, descripcion, email, cuit, sitioweb, idrubro, nombre: nombreRubro } = row;
@@ -57,7 +56,7 @@ const selectProveedoresYRubros = async () => {
             });
         });
 
-        // Convertir el objeto JSON en un arreglo de proveedores
+        // a json
         const proveedoresArray = Object.values(proveedoresConRubros);
 
         return proveedoresArray;
